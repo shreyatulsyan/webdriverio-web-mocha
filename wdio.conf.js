e917115279ae064958eef1ea9e17025b02831629
shreya.tulsyan <shreyatulsyan01@gmail.com>
2023-11-19 18:30:59 +0530

    changes in oncomplete hook

--- wdio.conf.js
+++ wdio.conf.js
@@ -164,24 +164,24 @@ exports.config = {
         console.log('onComplete =>');
         const mergeResults = require('wdio-mochawesome-reporter/mergeResults')
         mergeResults('./MochaAwesome-results', "results-*","merged.json")
-        const allure = require('allure-commandline')
-        const reportError = new Error('Could not generate Allure report')
-        const generation = allure(['generate', 'allure-results', '--clean'])
-        return new Promise((resolve, reject) => {
-            const generationTimeout = setTimeout(
-                () => reject(reportError),
-                5000)
+        // const allure = require('allure-commandline')
+        // const reportError = new Error('Could not generate Allure report')
+        // const generation = allure(['generate', 'allure-results', '--clean'])
+        // return new Promise((resolve, reject) => {
+        //     const generationTimeout = setTimeout(
+        //         () => reject(reportError),
+        //         5000)
 
-            generation.on('exit', function(exitCode) {
-                clearTimeout(generationTimeout)
+        //     generation.on('exit', function(exitCode) {
+        //         clearTimeout(generationTimeout)
 
-                if (exitCode !== 0) {
-                    return reject(reportError)
-                }
+        //         if (exitCode !== 0) {
+        //             return reject(reportError)
+        //         }
 
-                console.log('Allure report successfully generated')
-                resolve()
-            }) })
+        //         console.log('Allure report successfully generated')
+        //         resolve()
+        //     }) })
 
         
       },
