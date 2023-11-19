67eef4f12054d51640bccb025cc4b69340957e06
shreya.tulsyan <shreyatulsyan01@gmail.com>
2023-11-19 21:08:28 +0530

    Fixed allure report in jenkins

--- test/specs/horizontalSlider.js
+++ test/specs/horizontalSlider.js
@@ -1,12 +1,12 @@
 const horizontalSliderPage = require("../pageobjects/horizontalSlider.page")
 const loginPage = require("../pageobjects/login.page")
 let {Key} = require('webdriverio');
-// describe('Horizontal Slider by pressing keys', async()=>{
-//     it('Sliding horizontally by 3', async()=>{
-//         loginPage.open('horizontal_slider')
-//         await (horizontalSliderPage.slider).click({ button: 1 });
-//         await browser.keys([Key.ArrowRight,Key.ArrowRight,Key.ArrowRight]);
-//         await expect(horizontalSliderPage.rangeValue).toHaveText('1.5');
-//         await browser.saveScreenshot('Screenshots/Slider.png')
-//     })
-// })
+describe('Horizontal Slider by pressing keys', async()=>{
+    it('Sliding horizontally by 3', async()=>{
+        loginPage.open('horizontal_slider')
+        await (horizontalSliderPage.slider).click({ button: 1 });
+        await browser.keys([Key.ArrowRight,Key.ArrowRight,Key.ArrowRight]);
+        await expect(horizontalSliderPage.rangeValue).toHaveText('1.5');
+        await browser.saveScreenshot('Screenshots/Slider.png')
+    })
+})
